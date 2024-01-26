c2dfde49f98f8de0d2f71a25270b610251f7c941
assanalizz <154741138+assanalizz@users.noreply.github.com>
2024-01-27 03:32:32 +0600

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,40 +1,44 @@
 public class Student extends Person {
-    private double gpa;
+    private double gpa; // GPA (Grade Point Average) of the student
 
     public Student() {
-        super();
+        super(); // Call the default constructor of the superclass (Person)
     }
 
+    // Parameterized constructor with name, surname, and GPA
     public Student(String name, String surname, double gpa) {
-        super(name, surname);
-        this.gpa = gpa;
+        super(name, surname); // Call the parameterized constructor of the superclass (Person)
+        this.gpa = gpa; // Set the GPA for the student
     }
 
     public double getGpa() {
-        return gpa;
+        return gpa; // Return the GPA of the student
     }
 
     public void setGpa(double gpa) {
-        this.gpa = gpa;
+        this.gpa = gpa; // Set the GPA of the student
     }
 
     @Override
     public double getPaymentAmount() {
-        // Stipend is given in case student has gpa > 2.67
+        // Stipend is given in case the student has a GPA greater than 2.67
         return (gpa > 2.67) ? 36660.00 : 0.0;
     }
 
     @Override
     public String toString() {
-        return "Student: " + super.toString();
+        // Return a string representation of the Student object
+        return "Student: " + super.toString(); // Call the toString method of the superclass and append "Student: "
     }
 
     @Override
     public int compareTo(Person o) {
         if (o instanceof Student) {
+            // If the comparison object is also a Student, compare based on payment amount
             Student otherStudent = (Student) o;
             return Double.compare(this.getPaymentAmount(), otherStudent.getPaymentAmount());
         }
+        // If the comparison object is not a Student, use the default comparison from the superclass
         return super.compareTo(o);
     }
 }
