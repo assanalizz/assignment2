0354e765a469d83c6c5f8c063266df40f7f69593
assanalizz <154741138+assanalizz@users.noreply.github.com>
2024-01-27 02:27:57 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ public class Main {
         List<Person> people = new ArrayList<>();
 
         // Add several employees and students
-        people.add(new Employee("Cristiano", "Ronaldo", "Manager", 2700.78));
-        people.add(new Student("Victor", "Tsoy", 3.7));
-        people.add(new Student("Nursultan", "Khaimuldin", 0.07));
-        people.add(new Employee("George", "Harrison", "Developer", 50000.00));
+        people.add(new Employee("Donald", "Trump", "Manager", 2700.78));
+        people.add(new Student("Will", "Smith", 3.99));
+        people.add(new Student("Alexander", "Lukashenko", 0.08));
+        people.add(new Employee("Kanye", "West", "Developer", 80990.00));
 
         // Sort the list based on the amount of money they make
         Collections.sort(people);
