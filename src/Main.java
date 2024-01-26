d15e66264aab441737149289048a34d484574a98
assanalizz <154741138+assanalizz@users.noreply.github.com>
2024-01-27 03:25:47 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+// Main class to demonstrate sorting of Person objects
 public class Main {
 
     public static void main(String[] args) {
+        // Create a list to store Person objects
         List<Person> people = new ArrayList<>();
 
-        // Add several employees and students
-        people.add(new Employee("Donald", "Trump", "Manager", 2700.78));
-        people.add(new Student("Will", "Smith", 3.99));
-        people.add(new Student("Alexander", "Lukashenko", 0.08));
-        people.add(new Employee("Kanye", "West", "Developer", 80990.00));
+        // Add several employees and students to the list
+        people.add(new Employee("Kurt", "Cobain", "Manager", 270000.00));
+        people.add(new Student("Kairat", "Nurtas", 3.99));
+        people.add(new Student("Jon", "Jones", 1.2));
+        people.add(new Employee("Kanye", "West", "Developer", 809900.00));
 
-        // Sort the list based on the amount of money they make
+        // Sort the list based on the natural order defined in the Person class
         Collections.sort(people);
 
-        // Call printData method
+        // Call the printData method to display information about each person
         printData(people);
     }
 
+    // Method to print information about each person in the provided Iterable
     public static void printData(Iterable<Person> people) {
+        // Iterate through the collection and print details for each person
         for (Person person : people) {
             System.out.println(person + " earns " + person.getPaymentAmount() + " tenge");
         }
